37dc608f42a68a0e699534bfd26974f899a66589
Eduardo Andreetta Fontana <fontanadnb@gmail.com>
2017-12-17 17:02:01 -0200

    Changed to return model on GET verb, not the entity.

--- SwarmServerAPI/Controllers/SessionController.cs
+++ SwarmServerAPI/Controllers/SessionController.cs
@@ -16,13 +16,88 @@ namespace SwarmServerAPI.Controllers
         //Best practices
         //https://blog.mwaysolutions.com/2014/06/05/10-best-practices-for-better-restful-api/
 
-        public IEnumerable<Session> Get()
+        public IEnumerable<SessionModel> Get()
         {
             try
             {
                 using (SwarmData context = new SwarmData())
                 {
-                    return context.Sessions.ToList();
+                    return context.Sessions.Select(s => new SessionModel
+                    {
+                        Identifier = s.Identifier,
+                        Label = s.Label,
+                        Description = s.Description,
+                        Purpose = s.Purpose,
+                        Started = s.Started,
+                        Finished = s.Finished ?? DateTime.MinValue,
+                        Task = new TaskModel
+                        {
+                            Action = s.Task.Action,
+                            Created = s.Task.Created,
+                            Description = s.Task.Description,
+                            Name = s.Task.Name,
+                            Project = new ProjectModel
+                            {
+                                Name = s.Task.Project.Name,
+                                Description = s.Task.Project.Description
+                            }
+                        },
+                        Developer = new DeveloperModel
+                        {
+                            Name = s.Developer.Name
+                        },
+                        Breakpoints = s.Breakpoints.Select(b => new BreakpointModel
+                        {
+                            BreakpointKind = b.BreakpointKind,
+                            Created = b.Created,
+                            LineNumber = b.LineNumber ?? 0,
+                            LineOfCode = b.LineOfCode,
+                            Namespace = b.Namespace,
+                            Origin = b.Origin,
+                            Type = b.Type
+                        }).ToList(),
+                        Events = s.Events.Select(e => new EventModel
+                        {
+                            CharEnd = e.CharEnd ?? 0,
+                            CharStart = e.CharStart ?? 0,
+                            Created = e.Created,
+                            Detail = e.Detail,
+                            EventKind = e.EventKind,
+                            LineNumber = e.LineNumber ?? 0,
+                            LineOfCode = e.LineOfCode,
+                            Method = e.Method,
+                            MethodKey = e.MethodKey,
+                            MethodSignature = e.MethodSignature,
+                            Namespace = e.Namespace,
+                            Type = e.Type,
+                            TypeFullPath = e.TypeFullPath
+                        }).ToList(),
+                        PathNodes = s.PathNodes.Select(p => new PathNodeModel
+                        {
+                            Created = p.Created,
+                            Type = p.Type,
+                            Namespace = p.Namespace,
+                            Hash = p.Hash,
+                            Method = p.Method,
+                            MethodCodeMetric = new CodeMetricModel
+                            {
+                                ClassCoupling = p.MethodCodeMetric.ClassCoupling,
+                                CyclomaticComplexity = p.MethodCodeMetric.CyclomaticComplexity,
+                                Hash = p.MethodCodeMetric.Hash,
+                                LineOfCode = p.MethodCodeMetric.LineOfCode,
+                                MaintainabilityIndex = p.MethodCodeMetric.MaintainabilityIndex
+                            },
+                            Origin = p.Origin,
+                            Parameters = p.Parameters.Select(pp => new PathNodeParameterModel
+                            {
+                                Name = pp.Name,
+                                Type = pp.Type,
+                                Value = pp.Value
+                            }).ToList(),
+                            Parent = p.Parent,
+                            ReturnType =p.ReturnType
+                        }).ToList()
+                    }).ToList();
                 }
             }
             catch (Exception ex)
