c41ae3042c3e5c9d33f5d056360e9a2bc1b54bf2
Eduardo Andreetta Fontana <fontanadnb@gmail.com>
2017-12-21 20:37:41 -0200

    Added ag-grid datagrid javascript test.

--- SwarmServerAPI/App_Start/BundleConfig.cs
+++ SwarmServerAPI/App_Start/BundleConfig.cs
@@ -20,6 +20,9 @@ namespace SwarmServerAPI
                       "~/Scripts/bootstrap.js",
                       "~/Scripts/respond.js"));
 
+            bundles.Add(new ScriptBundle("~/bundles/data-session").Include(
+                      "~/Scripts/data-session.js"));
+
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
