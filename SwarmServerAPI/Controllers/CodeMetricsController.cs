f79bb52ae6d14fbd2bfbd7e1fa327704c7fca37c
Eduardo Andreetta Fontana <fontanadnb@gmail.com>
2017-12-22 17:15:10 -0200

    Added CodeMetrics page.

---
+++ SwarmServerAPI/Controllers/CodeMetricsController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SwarmServerAPI.Controllers
+{
+    public class CodeMetricsController : Controller
+    {
+        // GET: CodeMetrics
+        public ActionResult Index()
+        {
+            ViewBag.Title = "Code Metrics";
+
+            return View();
+        }
+    }
+}
