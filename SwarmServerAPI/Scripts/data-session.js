d378f351efb9fdce34c5b70170b17931d8cc93f2
Eduardo Andreetta Fontana <fontanadnb@gmail.com>
2017-12-22 14:14:40 -0200

    Configured basics ui resources on session data grid.

--- SwarmServerAPI/Scripts/data-session.js
+++ SwarmServerAPI/Scripts/data-session.js
@@ -14,8 +14,25 @@ var gridOptions = {
     rowData: [],
     onGridReady: function () {
         gridOptions.api.sizeColumnsToFit();
-    }
+    },
+    rowSelection: 'multiple',
+    enableColResize: true,
+    enableSorting: true,
+    enableFilter: true,
+    enableRangeSelection: true,
+    suppressRowClickSelection: true,
+    animateRows: true,
+    onModelUpdated: modelUpdated,
+    debug: true
 };
+
+function modelUpdated() {
+    var model = gridOptions.api.getModel();
+    var totalRows = model.getTopLevelNodes().length;
+    var processedRows = model.getRowCount();
+    var eSpan = document.querySelector('#rowCount');
+    eSpan.innerHTML = processedRows.toLocaleString() + ' / ' + totalRows.toLocaleString();
+}
 
 // used in our jasmine test
 function selectAllRows() {
@@ -28,7 +45,7 @@ $(function () {
     $.get("api/sessiongriddata", function (data) {
         self.gridOptions.rowData = data;
 
-        var eGridDiv = document.querySelector('#myGrid');
+        var eGridDiv = document.querySelector('#sessionDataGrid');
 
         new agGrid.Grid(eGridDiv, gridOptions);
     });
