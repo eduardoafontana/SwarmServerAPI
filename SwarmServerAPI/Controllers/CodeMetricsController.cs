93d13945af131a6d3ca26ba709759793dc3e5004
Eduardo Andreetta Fontana <fontanadnb@gmail.com>
2017-12-22 17:32:21 -0200

    Added upload file form on code metrics page.

--- SwarmServerAPI/Controllers/CodeMetricsController.cs
+++ SwarmServerAPI/Controllers/CodeMetricsController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -14,6 +15,24 @@ namespace SwarmServerAPI.Controllers
             ViewBag.Title = "Code Metrics";
 
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult Upload()
+        {
+            if (Request.Files.Count > 0)
+            {
+                var file = Request.Files[0];
+
+                if (file != null && file.ContentLength > 0)
+                {
+                    var fileName = Path.GetFileName(file.FileName);
+                    var path = Path.Combine(Server.MapPath("~/Images/"), fileName);
+                    //file.SaveAs(path);
+                }
+            }
+
+            return RedirectToAction("Index");
         }
     }
 }
