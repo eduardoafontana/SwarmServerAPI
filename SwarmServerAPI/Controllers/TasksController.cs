a850f076b40c877ce06db38075ecf7139cdba32a
Eduardo Andreetta Fontana <fontanadnb@gmail.com>
2017-12-23 15:22:38 -0200

    Created tasks page.

---
+++ SwarmServerAPI/Controllers/TasksController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SwarmServerAPI.Controllers
+{
+    public class TasksController : Controller
+    {
+        // GET: Tasks
+        public ActionResult Index()
+        {
+            ViewBag.Title = "Tasks";
+
+            return View();
+        }
+    }
+}
