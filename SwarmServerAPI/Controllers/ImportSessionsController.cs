fcc9ea47086161c9a1ccd4a9ffa6aa765d8bb953
Eduardo Andreetta Fontana <fontanadnb@gmail.com>
2017-12-23 22:44:41 -0200

    Created import session page and added multiple file upload.

---
+++ SwarmServerAPI/Controllers/ImportSessionsController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SwarmServerAPI.Controllers
+{
+    public class ImportSessionsController : Controller
+    {
+        // GET: ImportSessions
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(HttpPostedFileBase[] files)
+        {
+            if (!ModelState.IsValid)
+                return View();
+
+            int fileSuccessfullyUploaded = 0;
+
+            foreach (HttpPostedFileBase file in files)
+            {
+                if (file == null)
+                    continue;
+
+                if (file.ContentLength <= 0)
+                    continue;
+
+                if (String.IsNullOrWhiteSpace(file.FileName))
+                    continue;
+
+                if (!Path.GetExtension(file.FileName).Equals(".txt"))
+                    continue;
+
+                if (file.FileName.Length != 29)
+                    continue;
+
+                if (!file.FileName.StartsWith("session-"))
+                    continue;
+
+                fileSuccessfullyUploaded++;
+
+                //var InputFileName = Path.GetFileName(file.FileName);
+                //var ServerSavePath = Path.Combine(Server.MapPath("~/UploadedFiles/") + InputFileName);
+                ////Save file to server folder  
+                //file.SaveAs(ServerSavePath);
+            }
+
+            //assigning file uploaded status to ViewBag for showing message to user.  
+            ViewBag.UploadStatus = fileSuccessfullyUploaded + " files uploaded successfully.";
+
+            return View();
+        }
+    }
+}
