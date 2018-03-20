64c93ff3d9c8465cab22a37eaa39a9c343e4282a
Eduardo Andreetta Fontana <fontanadnb@gmail.com>
2018-03-19 21:23:47 -0300

    Changed to a singleton strategy to process requests asynchronously.

--- SwarmServerAPI/Controllers/Api/SessionController.cs
+++ SwarmServerAPI/Controllers/Api/SessionController.cs
@@ -35,55 +35,101 @@ namespace SwarmServerAPI.UI.SwarmServerAPI.Controllers
             }
         }
 
-        private static object syncPost = new Object();
+        public sealed class SessionSingleton
+        {
+            private static volatile SessionSingleton instance;
+            private static object syncRoot = new Object();
+
+            public Queue<Session> PostSessionList { get; set; } = new Queue<Session>();
+
+            private SessionSingleton() { }
+
+            public static SessionSingleton Instance
+            {
+                get
+                {
+                    if (instance != null)
+                        return instance;
+
+                    lock (syncRoot)
+                    {
+                        if (instance == null)
+                            instance = new SessionSingleton();
+                    }
+
+                    return instance;
+                }
+            }
+
+            public async System.Threading.Tasks.Task ProcessPosting()
+            {
+                await System.Threading.Tasks.Task.Run(() =>
+                {
+                    lock (PostSessionList)
+                    {
+                        using (SwarmData context = new SwarmData())
+                        {
+                            while (PostSessionList.Count > 0)
+                            {
+                                Session session = PostSessionList.Dequeue();
+
+                                if (session == null)
+                                    continue;
+
+                                if (session.Id == new Guid("00000000-0000-0000-0000-000000000000"))
+                                    continue;
+
+                                Session original = context.Sessions
+                                        .Include("Breakpoints")
+                                        .Include("Events")
+                                        .Include("PathNodes")
+                                        .FirstOrDefault(s => s.Id == session.Id);
+
+                                if (original == null)
+                                    context.Sessions.Add(session);
+                                else
+                                {
+                                    context.Entry(original).CurrentValues.SetValues(session);
+
+                                    foreach (Breakpoint item in session.Breakpoints)
+                                    {
+                                        if (!original.Breakpoints.Any(x => x.Id == item.Id))
+                                            original.Breakpoints.Add(item);
+                                    }
+
+                                    foreach (Event item in session.Events)
+                                    {
+                                        if (!original.Events.Any(x => x.Id == item.Id))
+                                            original.Events.Add(item);
+                                    }
+
+                                    foreach (PathNode item in session.PathNodes)
+                                    {
+                                        if (!original.PathNodes.Any(x => x.Id == item.Id))
+                                            original.PathNodes.Add(item);
+                                    }
+                                }
+
+                                context.SaveChanges();
+                            }
+                        }
+                    }
+                });
+            }
+        }
 
         public string Post(Session session)
         {
             try
             {
-                lock (syncPost)
-                {
-                    using (SwarmData context = new SwarmData())
-                    {
-                        if (session.Id == new Guid("00000000-0000-0000-0000-000000000000"))
-                            return "Post rejected: session.Identifier equal 00000000-0000-0000-0000-000000000000.";
+                if (session.Id == new Guid("00000000-0000-0000-0000-000000000000"))
+                    return "Post rejected: session.Identifier equal 00000000-0000-0000-0000-000000000000.";
 
-                        Session original = context.Sessions
-                            .Include("Breakpoints")
-                            .Include("Events")
-                            .Include("PathNodes")
-                            .FirstOrDefault(s => s.Id == session.Id);
+                SessionSingleton.Instance.PostSessionList.Enqueue(session);
 
-                        if (original == null)
-                            context.Sessions.Add(session);
-                        else
-                        {
-                            context.Entry(original).CurrentValues.SetValues(session);
+                SessionSingleton.Instance.ProcessPosting();
 
-                            foreach (Breakpoint item in session.Breakpoints)
-                            {
-                                if (!original.Breakpoints.Any(x => x.Id == item.Id))
-                                    original.Breakpoints.Add(item);
-                            }
-
-                            foreach (Event item in session.Events)
-                            {
-                                if (!original.Events.Any(x => x.Id == item.Id))
-                                    original.Events.Add(item);
-                            }
-
-                            foreach (PathNode item in session.PathNodes)
-                            {
-                                if (!original.PathNodes.Any(x => x.Id == item.Id))
-                                    original.PathNodes.Add(item);
-                            }
-                        }
-
-                        context.SaveChanges();
-
-                        return "Object created or updated!";
-                    }
-                }
+                return "Object created or updated!";
             }
             catch (Exception ex)
             {
