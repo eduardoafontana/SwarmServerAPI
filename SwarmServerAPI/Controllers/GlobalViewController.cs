c12476c75155bd25a4294f1aecbff6546bbc84ad
Eduardo Andreetta Fontana <fontanadnb@gmail.com>
2018-04-11 22:13:54 -0300

    Created global view page and menu item.

---
+++ SwarmServerAPI/Controllers/GlobalViewController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Web;
+using System.Web.Mvc;
+using System.Xml.Linq;
+using ICSharpCode.SharpZipLib.Zip;
+using SwarmServerAPI.AppCode.Repository;
+
+namespace SwarmServerAPI.UI.SwarmServerAPI.Controllers
+{
+    public class GlobalViewController : Controller
+    {
+        // GET: CodeMetrics
+        public ActionResult Index()
+        {
+            ViewBag.Title = "Global Views";
+
+            return View();
+        }
+    }
+}
