52af910d513917c925d2767452a0cb9ef1bb7eea
Eduardo Andreetta Fontana <fontanadnb@gmail.com>
2018-04-15 17:58:18 -0300

    Added session view inside the modal window on session listgrid.

--- SwarmServerAPI/Controllers/Api/SessionGridDataController.cs
+++ SwarmServerAPI/Controllers/Api/SessionGridDataController.cs
@@ -12,6 +12,8 @@ namespace SwarmServerAPI.UI.SwarmServerAPI.Controllers
 {
     public class SessionGridDataController : ApiController
     {
+        [HttpGet]
+        [Route("api/SessionGridData")]
         public IEnumerable<SessionGridModel> Get()
         {
 
@@ -38,6 +40,66 @@ namespace SwarmServerAPI.UI.SwarmServerAPI.Controllers
             {
                 throw InternalError.ThrowError(ex);
             }
+        }
+
+        [HttpGet]
+        [Route("api/SessionGridData/SessionVisualization/{id}")]
+        public List<ElementModel.Element> GetSessionVisualization(string id)
+        {
+            ElementModel model = new ElementModel();
+            List<PathNode> pnCollection = new List<PathNode>();
+
+            using (SwarmData context = new SwarmData())
+            {
+                pnCollection = context.PathNodes
+                    .Where(pn => pn.Session.Id.ToString() == id).OrderBy(pn => pn.Created).ToList();
+            }
+
+            //load nodes
+            foreach (PathNode pn in pnCollection)
+            {
+                model.ElementCollection.Add(new ElementModel.Element()
+                {
+                    data = new ElementModel.Data()
+                    {
+                        id = pn.Id.ToString(),
+                        parent_id = pn.Parent_Id.ToString(),
+                        method = pn.Method,
+                        nodeinfo = new ElementModel.NodeInfo()
+                        {
+                            name_space = pn.Namespace,
+                            type = pn.Type,
+                            method = pn.Method,
+                            returntype = pn.ReturnType,
+                            origin = pn.Origin,
+                            created = pn.Created.ToShortDateString()
+                        }
+                    }
+                });
+            }
+
+            //load edges
+            List<ElementModel.Element> edgesCollection = new List<ElementModel.Element>();
+
+            foreach (ElementModel.Element element in model.ElementCollection)
+            {
+                if (element.data.parent_id == Guid.Empty.ToString())
+                    continue;
+
+                edgesCollection.Add(new ElementModel.Element()
+                {
+                    data = new ElementModel.Data()
+                    {
+                        id = element.data.id + "-" + element.data.id,
+                        source = element.data.parent_id,
+                        target = element.data.id
+                    }
+                });
+            }
+
+            model.ElementCollection.AddRange(edgesCollection);
+
+            return model.ElementCollection;
         }
     }
 }
