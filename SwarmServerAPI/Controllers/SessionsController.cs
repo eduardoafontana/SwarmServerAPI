7225e2f42551dbcb89b0debc0e037771da9e396d
Eduardo Andreetta Fontana <fontanadnb@gmail.com>
2018-04-16 21:40:12 -0300

    Created session page.

---
+++ SwarmServerAPI/Controllers/SessionsController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SwarmServerAPI.UI.SwarmServerAPI.Views
+{
+    public class SessionsController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Title = "Sessions Page";
+
+            return View();
+        }
+    }
+}
