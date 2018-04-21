ab7eb188f518e81250615697f6b0579fb9cd8276
Eduardo Andreetta Fontana <fontanadnb@gmail.com>
2018-04-20 22:00:47 -0300

    Changed data grid to support no scroll mode.

--- SwarmServerAPI/Scripts/data-session.js
+++ SwarmServerAPI/Scripts/data-session.js
@@ -69,6 +69,40 @@ function modelUpdated() {
     var processedRows = model.getRowCount();
     var eSpan = document.querySelector('#rowCount');
     eSpan.innerHTML = processedRows.toLocaleString() + ' / ' + totalRows.toLocaleString();
+
+    setDynamicGridHeight(processedRows);
+}
+
+function setDynamicGridHeight(currentRowsCount) {
+    var offSet = 1.005;
+
+    if (currentRowsCount <= 700)
+        offSet = 1.01;
+
+    if (currentRowsCount <= 250)
+        offSet = 1.02;
+
+    if (currentRowsCount <= 120)
+        offSet = 1.04;
+
+    if (currentRowsCount <= 60)
+        offSet = 1.05; 
+
+    if (currentRowsCount <= 30)
+        offSet = 1.1;
+
+    if (currentRowsCount <= 20)
+        offSet = 1.2;
+
+    if (currentRowsCount <= 10)
+        offSet = 1.3;
+
+    var height = currentRowsCount * 25 * offSet;
+
+    if (currentRowsCount <= 5)
+        height = 300;
+
+    $('.ag-body, .ag-scrolls').height(height);
 }
 
 function addQuickFilterListener() {
