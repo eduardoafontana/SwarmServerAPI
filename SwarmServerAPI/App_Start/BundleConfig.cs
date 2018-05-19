6dbd1c390ec7df4bfa9e4065b5686916799de691
Eduardo Andreetta Fontana <fontanadnb@gmail.com>
2018-05-18 21:19:42 -0300

    Bug fix on load pooperjs.

--- SwarmServerAPI/App_Start/BundleConfig.cs
+++ SwarmServerAPI/App_Start/BundleConfig.cs
@@ -17,7 +17,7 @@ namespace SwarmServerAPI.UI.SwarmServerAPI
                         "~/Scripts/modernizr-*"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/popper.js",
+                      "~/Scripts/umd/popper.js",
                       "~/Scripts/bootstrap.js",
                       "~/Scripts/respond.js",
                       "~/Scripts/bootstrap-select.js",
