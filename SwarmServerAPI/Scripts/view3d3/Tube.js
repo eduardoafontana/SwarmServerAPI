06a9e557b746ccfa34002edf3e3b8b1a07213e2e
Eduardo Andreetta Fontana <fontanadnb@gmail.com>
2018-12-17 21:00:44 -0200

    Added Tube segment to new formation.

--- SwarmServerAPI/Scripts/view3d3/Tube.js
+++ SwarmServerAPI/Scripts/view3d3/Tube.js
@@ -1,17 +1,21 @@
 ﻿var Tube = function (nodes) {
 
     var vertices = [];
-    var marginBottom = 0.5;
+    var topHeightMargin = -0.25;
+    var topHeightMarginVariation = topHeightMargin + groupAssembler.getPositionTopBase();
 
     for (var i = 0; i < nodes.length; i++) {
-        var height = nodes[i].line * 50 / groupAssembler.getMostHighFileLine();
-        var heightWithMargin = height + marginBottom;
+        var height = i; 
+        var heightAdjustment = height * (-1);
 
-        vertices.push(new THREE.Vector3(nodes[i].x * 1.5, heightWithMargin, nodes[i].z));
+        var heightWithMargin = heightAdjustment + topHeightMarginVariation;
+
+        vertices.push(new THREE.Vector3(nodes[i].x * 3, heightWithMargin, nodes[i].z));
     }
 
     var curve = new THREE.CatmullRomCurve3(vertices, false, 'catmullrom', 0.2);
     var geometry = new THREE.TubeGeometry(curve, 20, 0.05, 5, false);
+    //TODO: review 20 and 5 on parameter in TubeGeometry
     var material = new THREE.MeshBasicMaterial();
     var mesh = new THREE.Mesh(geometry, material);
 
@@ -38,7 +42,7 @@
             mesh.geometry.vertices[i].x = originalVertices[i].x * render.getSelectedScene().scaleOptions.options.cubeSpace;
             mesh.geometry.vertices[i].z = originalVertices[i].z * render.getSelectedScene().scaleOptions.options.sessionSpace;
 
-            mesh.geometry.vertices[i].y = ((originalVertices[i].y - marginBottom) * render.getSelectedScene().scaleOptions.options.heightScale) + marginBottom;
+            mesh.geometry.vertices[i].y = ((originalVertices[i].y - topHeightMarginVariation) * render.getSelectedScene().scaleOptions.options.heightScale) + topHeightMarginVariation;
         }
 
         mesh.geometry.verticesNeedUpdate = true;
