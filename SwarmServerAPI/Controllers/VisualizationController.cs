c1bdce14fff28cfd64a9b9e1cb258d0db450c09b
Eduardo Andreetta Fontana <fontanadnb@gmail.com>
2020-01-26 10:46:27 -0300

    Refactoring. Removed old module ImportSession.

--- SwarmServerAPI/Controllers/VisualizationController.cs
+++ SwarmServerAPI/Controllers/VisualizationController.cs
@@ -4,7 +4,6 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using SwarmServerAPI.AppCode.Repository;
-using SwarmServerAPI.UI.SwarmServerAPI.Models;
 
 namespace SwarmServerAPI.UI.SwarmServerAPI.Controllers
 {
